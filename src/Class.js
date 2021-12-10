ae81765c65f945ffa8a223cd9e424b5b09e53602
Cody <relationalprogrammer@gmail.com>
2021-12-10 10:47:44 -0600

    finished building components

--- src/Class.js
+++ src/Class.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Class extends Component {
     state = {
         text: ""
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleClick = event => {
+        this.setState({ text: "" })
     }
 
     render() {
@@ -10,8 +18,13 @@ export default class Class extends Component {
             <div className="form-container">
                 <fieldset>
                     <legend>Class Component</legend>
+                    <input type="text" onChange={this.handleChange} placeholder="Enter some text" name="text" value={this.state.text} />
                 </fieldset>
-                
+                <div className="output">
+                    <h3>Your Text Output</h3>
+                    <span>{this.state.text}</span>
+                </div>
+                <button onClick={this.handleClick}>Clear</button>
             </div>
         )
     }
--- src/Functional.js
+++ src/Functional.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function Functional() {
     const [text, setText] = useState("");
@@ -7,13 +7,21 @@ export default function Functional() {
         setText(event.target.value)
     }
 
+    const handleClick = event => {
+        setText("")
+    }
+
     return (
         <div className="form-container">
             <fieldset>
                 <legend>Functional Component</legend>
                 <input type="text" placeholder="Enter some text" onChange={handleChange} value={text} />
             </fieldset>
-            
+            <div className="output">
+                <h3>Your Text Output</h3>
+                <span>{text}</span>
+            </div>
+            <button onClick={handleClick}>Clear</button>
         </div>
     )
 }
